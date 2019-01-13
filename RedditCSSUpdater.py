46a850ca94808fc04701293f97e05af4e4dbd236
Michael Lah <michael@michaellah.com>
2019-01-13 00:21:23 -0500

    Initial commit

---
+++ RedditCSSUpdater.py
@@ -0,0 +1,36 @@
+import praw
+import threading
+
+# Configuration start
+username = 'Timbo_KZ' # Your reddit username
+password = 'notmyrealpassword' # Your reddit password
+subredditName = 'NarcissusCSS' # Suberddit name (you must be a moderator)
+stylesheetPath = 'D:/NarcissusCSS/css/stylesheet.css' # Path to your stylesheet
+clientID = 'ClientID' # Your app's ID
+clientSecret = 'ClientSecret' # You app's secret
+clientCallback = 'http://127.0.0.1:65010/authorize_callback'
+# Configuration end
+
+r = praw.Reddit('LiveStyle v0.1 by /u/Timbo_KZ')
+r.set_oauth_app_info(client_id=clientID, client_secret=clientSecret, redirect_uri=clientCallback)
+r.login(username, password)
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
+def static_var(varname, value):
+    def decorate(func):
+        setattr(func, varname, value)
+        return func
+    return decorate
+@static_var("oldStylesheet", "")
+def update_stylesheet():
+    threading.Timer(2.0, update_stylesheet).start()
+    print("Checking CSS...")
+    stylesheet = file_get_contents(stylesheetPath)
+    if stylesheet != update_stylesheet.oldStylesheet:
+        print("Updating CSS...")
+        r.set_stylesheet(subredditName, stylesheet)
+        update_stylesheet.oldStylesheet = stylesheet
+    else:
+        print("CSS is up to date.")
+update_stylesheet()
