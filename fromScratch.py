7984953cd6529bcd0a7e623a329f8418c83b56bb
Michael Lah <michael@michaellah.com>
2019-01-15 18:56:53 -0500

    Added weather API

--- fromScratch.py
+++ fromScratch.py
@@ -1,7 +1,20 @@
 import praw
+import requests, json
+import configparser
+config = configparser.ConfigParser()
+config.read('weatherApi.ini')
+my_config = config['weather']
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+city_name = 'Columbus'
+complete_url = base_url + "appid=" + my_config['api_key'] + "&zip=14602"
+
+columbus_weather = requests.get(complete_url)
+columbus_weather_json = columbus_weather.json()
+
 
 reddit = praw.Reddit('rit', user_agent='rit_site')
 print(reddit.user.me())
 stylesheet = reddit.subreddit('MyTestPostSub').stylesheet()
 new_sheet = stylesheet.stylesheet + '*{background-color: red;}'
-reddit.subreddit('MyTestPostSub').stylesheet.update(new_sheet)
+# reddit.subreddit('MyTestPostSub').stylesheet.update(new_sheet)
